c8ecd030a0aa9c84197d442dd90313740d7a1c0e
becarella <becarella@gmail.com>
2012-05-23 22:12:03 -0400

    gem application layout include's app js

--- spec/dummy/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-  
-  before_filter :register_partials if Rails.env.development?
-  
-  def register_partials
-    HandlebarsConfig.register_partials
-  end
-end
--- spec/dummy/app/controllers/home_controller.rb
+++ spec/dummy/app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomeController < LetMeIn::ApplicationController
   def index
   end
 end
